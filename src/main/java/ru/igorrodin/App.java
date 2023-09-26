68ffc9b0faada1b72399e9c2006ff17dc3b65b40
Igor Rodin <igreen1974@gmail.com>
2023-09-27 01:31:51 +0300

    Add maven project pom.xml

---
+++ src/main/java/ru/igorrodin/App.java
@@ -0,0 +1,13 @@
+package ru.igorrodin;
+
+import ru.igorrodin.hw1.calc.Calculator;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Calculator calc = new Calculator();
+    }
+}
---
+++ src/test/java/ru/igorrodin/AppTest.java
@@ -0,0 +1,10 @@
+package ru.igorrodin;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+}
